df939d7f629535bbb8de4f24de8f8fe90f1f9074
THEFLILUX <jorge.nicho@utec.edu.pe>
2019-05-10 13:16:27 -0500

    actualizacion

--- PC2/Arena.h
+++ PC2/Arena.h
@@ -5,14 +5,19 @@
 #ifndef PC2_ARENA_H
 #define PC2_ARENA_H
 
-#include "Fight.h"
 #include <vector>
+#include <iostream>
+#include <string>
+#include "Fight.h"
 
 class Arena {
     std::vector<Fight> fights;
+    std:: string filename;
 public:
+    Arena();
     void run(Fighter a, Fighter b);
     std::vector<Fight> get_fights();
+    void load_fights(std::string filename);
     void run_all();
 
 };
